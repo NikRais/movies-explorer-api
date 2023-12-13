b388f0069a2d93e49ff5c3ba192052a20159015a
Nikita Raisky <mikado.raisky@yandex.ru>
2023-12-13 16:38:24 +0500

    fixed app file and other

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const router = require('./routes');
+const limiter = require('./utils/rateLimiter');
 const cors = require('./middlewares/cors');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
 
 const {
-  PORT, NODE_ENV, MONGO_URL, MONGO_URL_DEV,
+  PORT, NODE_ENV, DB_URL, MONGO_URL_DEV,
 } = require('./utils/devConstants');
 
 const app = express();
 app.use(cors);
-app.use(requestLogger);
+app.use(limiter);
 app.use(helmet());
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : MONGO_URL_DEV, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+app.use(requestLogger);
 
 app.use(router);
 
@@ -33,6 +29,11 @@ app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`App listening at port ${PORT}`);
-});
+async function init() {
+  await mongoose.connect(NODE_ENV === 'production' ? DB_URL : MONGO_URL_DEV);
+  await app.listen(PORT, () => {
+    console.log(`App listening at port ${PORT}`);
+  });
+}
+
+init();
--- utils/rateLimiter.js
+++ utils/rateLimiter.js
@@ -1,8 +1,6 @@
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-  standardHeaders: true,
-  legacyHeaders: false,
   windowMs: 15 * 60 * 1000,
   max: 100,
 });

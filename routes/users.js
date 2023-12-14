ed257c3942d3ff55dd5d823e178e7a44314faa8c
Nikita Raisky <mikado.raisky@yandex.ru>
2023-12-14 15:48:54 +0500

    fixed cors file 10

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const userRouter = require('express').Router();
 const { getCurrentUser, updateUser } = require('../controllers/users');
 const { updateUserValidation } = require('../middlewares/validations');
 
-userRouter.get('/users/me', getCurrentUser);
-userRouter.patch('/users/me', updateUserValidation, updateUser);
+userRouter.get('/me', getCurrentUser);
+userRouter.patch('/me', updateUserValidation, updateUser);
 
 module.exports = userRouter;
